07a6f5d30a827fc7b33dfd4d840fbc01bf601606
Oles Malysh <olesmalish37@gmail.com>
2024-05-17 14:44:30 +0200

    fragment_details screen design developed

---
+++ app/src/main/java/com/example/moneymanager/DetailsFragment.kt
@@ -0,0 +1,26 @@
+package com.example.moneymanager
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.moneymanager.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+    private lateinit var binding: FragmentDetailsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentDetailsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
